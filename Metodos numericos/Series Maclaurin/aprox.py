2f20c7c4662af6c73e52064e7e2fd8c92412cb1f
Jorge Parra <13104@ulsachihuahua.edu.mx>
2024-02-09 23:43:33 -0700

    metodos numericos update

--- Metodos numericos/Series Maclaurin/aprox.py
+++ Metodos numericos/Series Maclaurin/aprox.py
@@ -10,23 +10,21 @@ relative_error_per = 0
 true_relative_error_per = 0
 previous_aprox = 0
 
-for i in range(iterations):
-    value_per_term = x**i / math.factorial(i)
+print("{:<12} {:<15} {:<10} {:<16} {:<6} {:<6}".format('iteraciones', 'valor termino', 'aprox', 'error verdadero', 'et', 'ea'))
 
+for i in range(iterations+1 ):
+    value_per_term = x**i / math.factorial(i)
+    aprox += value_per_term
     if previous_aprox == 0:
         true_relative_error_per = 0
     else:
-        true_relative_error_per = (aprox - previous_aprox)/aprox * 100
+        true_relative_error_per = ((aprox - previous_aprox)/aprox) * 100
 
-    aprox += value_per_term
     previous_aprox = aprox
-
     true_error = abs(true_value - aprox)
     relative_error_per = (true_error / true_value) * 100
     
-    #print the results
-    print(f"{i}, {value_per_term}, {aprox}, {true_error}, {relative_error_per}%, {true_relative_error_per}%")
-
-
+    #resultados
+    print("{:<12} {:<15.9f} {:<10.9f} {:<16.9f} {:<6.2f}% {:<6.4f}%".format(i, value_per_term, aprox, true_error, relative_error_per, true_relative_error_per))
 
 
